db2d9cea29fe3f806e587de4faca405eee66d02c
Yash Gokakkar <yashgokakkar@gmail.com>
2023-03-30 11:46:53 +0530

    Added some hooks

--- invc/src/App.js
+++ invc/src/App.js
@@ -6,28 +6,60 @@ import Table from "./components/Table";
 import Notes from "./components/Notes";
 import Footer from "./components/Footer";
 
+import { useState } from "react";
+
 function App() {
+  const [showInvoice, setShowInvoice] = useState(false);
+
   const handlePrint = () => {
     window.print();
   };
   return (
     <>
       <main className="m-5 p-5 xl:max-w-4xl xl:mx-auto bg-white rounded shadow">
-        <div>
-          <Header handlePrint={handlePrint} />
+        {showInvoice ? (
+          <div>
+            <Header handlePrint={handlePrint} />
 
-          <MainDetails />
+            <MainDetails />
 
-          <ClientDetails />
+            <ClientDetails />
 
-          <Dates />
+            <Dates />
 
-          <Table />
+            <Table />
 
-          <Notes />
+            <Notes />
 
-          <Footer />
-        </div>
+            <Footer />
+            <button
+              onClick={() => setShowInvoice(false)}
+              className="bg-blue-500 text-white font-bold py-2 px-8 rounded shadow border-2 border-blue-500 hover:bg-transparent hover:text-blue-500 transition-all duration-300"
+            >
+              Edit Information
+            </button>
+          </div>
+        ) : (
+          <>
+          {/*name, address, client name, client address,email, phone number, bank name, banck account number, account holder, website invoice number, invoice date, due date, table, notes */}
+            <div className="flex flex-col justify-center">
+              <input
+                type="text"
+                name="text"
+                id="text"
+                placeholder="Enter your name"
+                autoComplete="off"
+              ></input>
+
+              <button
+                onClick={() => setShowInvoice(true)}
+                className="bg-blue-500 text-white font-bold py-2 px-8 rounded shadow border-2 border-blue-500 hover:bg-transparent hover:text-blue-500 transition-all duration-300"
+              >
+                Preview Invoice
+              </button>
+            </div>
+          </>
+        )}
       </main>
     </>
   );
